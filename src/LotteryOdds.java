ae70a3bfcdf69bf7f11d53a1a34419c401a1bd6e
arballop <33101698+arballop@users.noreply.github.com>
2017-10-28 19:13:25 -0700

    Changed the code to evaluate the odds of winning the lottery efficiently

--- src/LotteryOdds.java
+++ src/LotteryOdds.java
@@ -1,29 +1,34 @@
+/**
+ * Assignment: Powerball is a multi-state lottery that has millions of players each week.  
+ * Let's calculate the odds that someone will select all of the right numbers to win the big jackpot!
+ * @author Paysen Arballo
+ * Date: 10/28/2017
+ * Due: 10/29/2017
+ */
 import java.math.BigInteger;
+import java.util.Scanner;
+
+
 
 public class LotteryOdds {
-
 	public static void main(String[] args) {
-
-		int r;
-		int w = 10; // 69
-		int s = 2; // 5
-		int pb = 26;
-	    r = factorial(w) / (factorial(s) * (factorial(w-s)));  // Superduper magic equation!!
-	    r = r * pb;
-	    
-	    System.out.println("The odds of winning the PowerBall are 1:" + r);
-	    System.out.println("The PowerBall website says that it's 1:292201338");
-	    
-    }
-
-	private static Integer factorial(int n) {
-	    int i = 1;
-	    int f = 1;
-	 
-	    for (int c = 1; c <= n; c++) {
-	      f = f * i++;
-	    }
-
-		return f;
+		Scanner in = new Scanner(System.in);
+		
+// Ask user for input credentials, so this code can compute for more than the initial assignment.
+		
+		System.out.print("How many numbers would you like to draw?");   // Ask user for number of selected balls
+		int k = in.nextInt();
+		
+		System.out.print("What is the pool size of the drawing?");     // Ask user for pool size to draw from
+		int n = in.nextInt();
+		
+		// Calculate binomial coefficient n		
+		BigInteger LotteryOdds =BigInteger.valueOf(1);                
+		for (int i= 1; i<= k; i++)
+			LotteryOdds = LotteryOdds.multiply(BigInteger.valueOf(n - i + 1)).divide(BigInteger.valueOf(i)); //Formula in correct form
+		
+		System.out.println("The odds of winning the lottery are 1 in "+LotteryOdds+". Good Luck!"); // Output is odds in winning the lottery
 	}
 }
+		
+		
